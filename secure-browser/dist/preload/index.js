3aace7173f111b7baa3d4558219ab5400c7ecbf1
sagar97189 <sagar020coc@gmail.com>
2026-05-25 13:13:00 +0530

    remove dist folder from tracking

--- secure-browser/dist/preload/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld("amanziSecureBrowser", {
-    platformLimitations: () => electron_1.ipcRenderer.invoke("secure-browser:platform-limitations"),
-    getEnvFlags: () => electron_1.ipcRenderer.invoke("secure-browser:get-env-flags"),
-    sendHeartbeat: () => electron_1.ipcRenderer.send("secure-browser:heartbeat"),
-});
